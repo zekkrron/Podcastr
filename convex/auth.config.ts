23bffb7d71cc94b22ef22495677880338a348ae8
Akash Singh <akash.singh13503@gmail.com>
2024-07-12 23:01:04 +0530

    first-commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,12 @@
+// https://direct-donkey-68.clerk.accounts.dev
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://direct-donkey-68.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+  
+  export default authConfig;
